6e9b2ed95581a1462789ad992fd348bd7ef55a19
Sergey <sharipov14@gmail.com>
2017-10-03 19:08:24 -0400

    Fix '/sendDelete'

--- index.js
+++ index.js
@@ -5,141 +5,139 @@ var countGet = 0, countPost = 0;
 
 var restify = require('restify')
 
-  // Get a persistence engine for the products
-  , productsSave = require('save')('products')
+// Get a persistence engine for the products
+    , productsSave = require('save')('products')
 
-  // Create the restify server
-  , server = restify.createServer({ product: SERVER_NAME})
+// Create the restify server
+    , server = restify.createServer({product: SERVER_NAME})
 
-  server.listen(PORT, HOST, function () {
-  console.log('Server %s listening at %s', server.product, server.url)
-  console.log('Resources:')
-  console.log(' /sendGet')
-  console.log(' /sendPost')
+server.listen(PORT, HOST, function () {
+    console.log('Server %s listening at %s', server.product, server.url)
+    console.log('Resources:')
+    console.log(' /sendGet')
+    console.log(' /sendPost')
 })
 
 server
-  // Allow the use of POST
-  .use(restify.fullResponse())
+// Allow the use of POST
+    .use(restify.fullResponse())
 
-  // Maps req.body to req.params so there is no switching between them
-  .use(restify.bodyParser())
+    // Maps req.body to req.params so there is no switching between them
+    .use(restify.bodyParser())
 
 // Get all products in the system
 server.get('/sendGet', function (req, res, next) {
-  countGet++;
-  console.log("Count get: "+countGet,"; Count post: " + countPost + ";");
-  // Find every entity within the given collection
-  productsSave.find({}, function (error, products) {
+    countGet++;
+    console.log("Count get: " + countGet, "; Count post: " + countPost + ";");
+    // Find every entity within the given collection
+    productsSave.find({}, function (error, products) {
 
-    // Return all of the products in the system
-    res.send(products)
-  })
+        // Return all of the products in the system
+        res.send(products)
+    })
 })
 
 // Get a single product by their product id
 server.get('/products/:id', function (req, res, next) {
 
-  // Find a single product by their id within save
-  productsSave.findOne({ _id: req.params.id }, function (error, product) {
+    // Find a single product by their id within save
+    productsSave.findOne({_id: req.params.id}, function (error, product) {
 
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+        // If there are any errors, pass them to next in the correct format
+        if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
-    if (product) {
-      // Send the product if no issues
-      res.send(product)
-    } else {
-      // Send 404 header if the product doesn't exist
-      res.send(404)
-    }
-  })
+        if (product) {
+            // Send the product if no issues
+            res.send(product)
+        } else {
+            // Send 404 header if the product doesn't exist
+            res.send(404)
+        }
+    })
 })
 
 server.del('/sendDelete', function (req, res, next) {
 
-  // Delete the product with the persistence engine
-  productsSave.delete( function (error, product) {
+    // Delete all products
+    productsSave.deleteMany(function (error, product) {
 
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+        // If there are any errors, pass them to next in the correct format
+        if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
-    // Send a 200 OK response
-    res.send()
-  })
+        // Send a 200 OK response
+        res.send(200)
+    })
 })
 
 // Create a new product
 server.post('/sendPost', function (req, res, next) {
-  countPost++;
-  console.log("Count get: "+countGet,"; Count post: " + countPost + ";");
-  // Make sure product is defined
-  if (req.params.product === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(new restify.InvalidArgumentError('product must be supplied'))
-  }
-  if (req.params.price === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(new restify.InvalidArgumentError('price must be supplied'))
-  }
-  var newProduct = {
-		product: req.params.product,
-		price: req.params.price
-	}
+    countPost++;
+    console.log("Count get: " + countGet, "; Count post: " + countPost + ";");
+    // Make sure product is defined
+    if (req.params.product === undefined) {
+        // If there are any errors, pass them to next in the correct format
+        return next(new restify.InvalidArgumentError('product must be supplied'))
+    }
+    if (req.params.price === undefined) {
+        // If there are any errors, pass them to next in the correct format
+        return next(new restify.InvalidArgumentError('price must be supplied'))
+    }
+    var newProduct = {
+        product: req.params.product,
+        price: req.params.price
+    }
 
-  // Create the product using the persistence engine
-  productsSave.create( newProduct, function (error, product) {
+    // Create the product using the persistence engine
+    productsSave.create(newProduct, function (error, product) {
 
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+        // If there are any errors, pass them to next in the correct format
+        if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
-    // Send the product if no issues
-    res.send(201, product)
-  })
+        // Send the product if no issues
+        res.send(201, product)
+    })
 })
 
 // Update a product by their id
 server.put('/products/:id', function (req, res, next) {
 
-  // Make sure product is defined
-  if (req.params.product === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(new restify.InvalidArgumentError('product must be supplied'))
-  }
-  if (req.params.price === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(new restify.InvalidArgumentError('price must be supplied'))
-  }
-  
-  var newproduct = {
-		_id: req.params.id,
+    // Make sure product is defined
+    if (req.params.product === undefined) {
+        // If there are any errors, pass them to next in the correct format
+        return next(new restify.InvalidArgumentError('product must be supplied'))
+    }
+    if (req.params.price === undefined) {
+        // If there are any errors, pass them to next in the correct format
+        return next(new restify.InvalidArgumentError('price must be supplied'))
+    }
+
+    var newproduct = {
+        _id: req.params.id,
         product: req.params.product,
         price: req.params.price
-	}
-  
-  // Update the product with the persistence engine
-  productsSave.update(newproduct, function (error, product) {
+    }
 
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+    // Update the product with the persistence engine
+    productsSave.update(newproduct, function (error, product) {
 
-    // Send a 200 OK response
-    res.send(200)
-  })
+        // If there are any errors, pass them to next in the correct format
+        if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+
+        // Send a 200 OK response
+        res.send(200)
+    })
 })
 
 // Delete product with the given id
 server.del('/products/:id', function (req, res, next) {
 
-  // Delete the product with the persistence engine
-  productsSave.delete(req.params.id, function (error, product) {
+    // Delete the product with the persistence engine
+    productsSave.delete(req.params.id, function (error, product) {
 
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+        // If there are any errors, pass them to next in the correct format
+        if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
-    // Send a 200 OK response
-    res.send()
-  })
+        // Send a 200 OK response
+        res.send()
+    })
 })
-
-
